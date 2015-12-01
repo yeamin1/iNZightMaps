87654f779ae110c7bf826f911dd1f1f393ff4544
Tom Elliott <tom.elliott@auckland.ac.nz>
2015-12-02 10:55:17 +1300

    specify global map obj

--- R/inzmap.R
+++ R/inzmap.R
@@ -2,17 +2,21 @@
 ##' @param obj an object from within iNZightPlot
 ##' @return Object
 ##' @author Tom Elliott
+##' @import iNZightPlots
+##' @import grid
+##' @export
 create.inz.mapplot <- function(obj) {
     out <- NextMethod()
 
     out$draw.axes <- FALSE
+    out$global.object <- list("map" = NULL)
     
     class(out) <- c("inzmap", class(out))
 
     out
 }
 
-
+##' @export
 plot.inzmap <- function(obj, gen) {
     xlim <- current.viewport()$xscale
     ylim <- current.viewport()$yscale
@@ -20,10 +24,23 @@ plot.inzmap <- function(obj, gen) {
     mcex <- gen$mcex
     col.args <- gen$col.args
 
+    map <- parent.frame()$global.object$map
+    get.newmap <- FALSE
+    if (is.null(map)) {
+        #get.newmap <- TRUE
+    } else {
+        ## check valid map
+    }
+
+    if (get.newmap) {
+        ## DL new map
+        map <- something()
+    }        
+        
     if (length(obj$x) == 0)
         return()
 
-    ptCols <- colourPoints(obj$colby, col.args, opts)
+    ptCols <- iNZightPlots:::colourPoints(obj$colby, col.args, opts)
 
     NotInView <- obj$x < min(xlim) | obj$x > max(xlim) | obj$y < min(ylim) | obj$y > max(ylim)
     obj$pch[NotInView] <- NA
